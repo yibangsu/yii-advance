5e3d9e21ed660866467bab81143dff0b4ead70c8
suyibang.fota <suyibang@hipad.com>
2018-09-28 04:45:51 -0400

    fix bug

--- frontend/config/main-local.php
+++ frontend/config/main-local.php
@@ -9,8 +9,8 @@ $config = [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=HiFota',
-            'username' => 'fotaadmin',
-            'password' => 'fotaadmin@123',
+            'username' => 'fotauser',
+            'password' => 'fotauser#123',
             'charset' => 'utf8',
         ],
     ],
@@ -21,11 +21,13 @@ if (!YII_ENV_TEST) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['10.1.152.228'], 
     ];
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['10.1.152.228'], 
     ];
 }
 
