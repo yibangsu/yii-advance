8de13d7d911d55a47c2a675aa6917ce718b9daaa
suyibang.fota <suyibang@hipad.com>
2018-09-28 04:47:32 -0400

    fix bug

--- frontend/config/params.php
+++ frontend/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
-    'fotaPackagePath' => '/data/suyibang/FotaPackages/',
-    'tempPackagePath' => '/data/suyibang/temp/',
+    'fotaPackagePath' => '/home/suyibang/FotaPackages/',
+    'tempPackagePath' => '/home/suyibang/temp/',
 ];
