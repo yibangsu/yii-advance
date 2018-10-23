1691e3414d956b71ccf2d0233e4bb2627ea1c1d5
suyibang <suyibang@hipad.com>
2018-10-23 16:01:01 +0800

    remove release note

--- frontend/views/software/_form.php
+++ frontend/views/software/_form.php
@@ -14,8 +14,6 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'sw_ver')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'sw_release_note')->textarea(['rows' => 6]) ?>
-
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>
