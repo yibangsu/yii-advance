21ec5f2f218ab662adb262d4acf327930a9fb06f
suyibang <suyibang@hipad.com>
2018-10-23 15:56:28 +0800

    remove sw_release_note

--- frontend/models/software/SoftwareSearch.php
+++ frontend/models/software/SoftwareSearch.php
@@ -67,7 +67,6 @@ class SoftwareSearch extends Software
         ]);
 
         $query->andFilterWhere(['like', 'sw_ver', $this->sw_ver])
-            ->andFilterWhere(['like', 'sw_release_note', $this->sw_release_note])
             ->andFilterWhere(['like', 'sw_date', $this->sw_date]);
 
         return $dataProvider;
